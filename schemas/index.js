1f8aa2cce580f2c420b5b1bd6e07050b20b66565
Jeayoeb Kim <jykim@atones.co.kr>
2022-01-01 23:23:37 +0900

    mongodb schema setting ing..

--- schemas/index.js
+++ schemas/index.js
@@ -1,9 +1,16 @@
 const mongoose = require('mongoose');
 
+// 몽구스 ODM을 쓰면 결국 RDB와 같이 제한된다
+// 개발자들이 원하는 것은 결국...
+
 const connect = () => {
+
+    // dev에서 debug 모드 사용
     if (process.env.NODE_ENV !== 'production') {
         mongoose.set('debug', true);
     }
+
+    // mongodb 프로토콜 사용
     mongoose.connect('mongodb://root:root@localhost:27017/admin', {
         dbName: 'nodejs',
         useNewUrlParser: true,
@@ -20,4 +27,8 @@ const connect = () => {
 mongoose.connection.on('error', (error) => {
     console.error('MongoDB Connection Error', error);
 });
+mongoose.connection.on('disconnected', () => {
+    console.error('MongoDB connection disconnected. Try connect.');
+})
 
+module.exports = connect();
