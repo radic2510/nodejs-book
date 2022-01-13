f37e5b27a774d88d0450d8cf12b6060695917306
Jeayoeb Kim <jykim@atones.co.kr>
2022-01-13 23:54:05 +0900

    sequelize model 선언 수정 도전중

--- chapter0-sequlize/models/user.js
+++ chapter0-sequlize/models/user.js
@@ -1,8 +1,59 @@
-const Sequelize = require('sequelize');
+const {DataTypes, Model} = require('sequelize');
 
-module.exports = class User extends Sequelize.Model {
+
+module.exports = class User extends Model {
     static init(sequelize) {
         return super.init({
+            name: {
+                type: DataTypes.STRING(20),
+                allowNull: false,
+                unique: true,
+            },
+            age: {
+                type: DataTypes.INTEGER.UNSIGNED,
+                allowNull: false,
+            },
+            married: {
+                type: DataTypes.BOOLEAN,
+                allowNull: false,
+            },
+            comment: {
+                type: DataTypes.TEXT,
+                allowNull: true,
+            },
+            created_at: {
+                type: DataTypes.DATE,
+                allowNull: false,
+                defaultValue: DataTypes.NOW,
+            },
+        }, {
+            sequelize,
+            timestamps: false,  //createdAt, updatedAt
+            underscored: false,
+            modelName: 'User',  // Post
+            tableName: 'users', // posts
+            paranoid: false,    //deletedAt : soft delete
+            charset: 'utf8',
+            collate: 'utf8_general_ci',
+        });
+    }
+
+    static associate(db) {
+        db.User.hasMany(db.Comment, { foreignKey: 'commenter', sourceKey: 'id' });
+    }
+};
+
+
+/*
+module.exports = (sequelize) => {
+    class User extends Model {
+        static associate(db) {
+            this.hasMany(db.Comment, { foreignKey: 'commenter', sourceKey: 'id' })
+        }
+    }
+
+    User.init(
+        {
             name: {
                 type: Sequelize.STRING(20),
                 allowNull: false,
@@ -24,7 +75,7 @@ module.exports = class User extends Sequelize.Model {
                 type: Sequelize.DATE,
                 allowNull: false,
                 defaultValue: Sequelize.NOW,
-            },
+            }
         }, {
             sequelize,
             timestamps: false,  //createdAt, updatedAt
@@ -34,10 +85,9 @@ module.exports = class User extends Sequelize.Model {
             paranoid: false,    //deletedAt : soft delete
             charset: 'utf8',
             collate: 'utf8_general_ci',
-        });
-    }
+        }
+    );
 
-    static associate(db) {
-        db.User.hasMany(db.Comment, { foreignKey: 'commenter', sourceKey: 'id' });
-    }
-};
+    return User;
+}
+*/
