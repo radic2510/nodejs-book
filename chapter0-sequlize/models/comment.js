b6a443f08e4f7dac0b8715d4fc0a5f5d531cfe6e
Jeayoeb Kim <jykim@atones.co.kr>
2022-01-14 15:03:36 +0900

    sequelize define 형태 정착

--- chapter0-sequlize/models/comment.js
+++ chapter0-sequlize/models/comment.js
@@ -1,29 +1,26 @@
-const {Sequelize, DataTypes, Model} = require('sequelize');
+const {DataTypes, Model} = require('sequelize');
 
 module.exports = class Comment extends Model {
-    static init(sequelize) {
-        return super.init({
-            comment: {
-                type: DataTypes.STRING(100),
-                allowNull: false,
-            },
-            created_at: {
-                type: DataTypes.DATE,
-                allowNull: true,
-                defaultValue: DataTypes.NOW,
-            },
-        }, {
-            sequelize,
-            timestamps: false,
-            modelName: 'Comment',
-            tableName: 'comments',
-            paranoid: false,
-            charset: 'utf8mb4',
-            collate: 'utf8mb4_general_ci',
-        });
-    }
+    static init = (sequelize) => super.init({
+        comment: {
+            type: DataTypes.STRING(100),
+            allowNull: false,
+        },
+        created_at: {
+            type: DataTypes.DATE,
+            allowNull: true,
+            defaultValue: DataTypes.NOW,
+        },
+    }, {
+        sequelize,
+        timestamps: false,
+        modelName: 'Comment',
+        tableName: 'comments',
+        paranoid: false,
+        charset: 'utf8mb4',
+        collate: 'utf8mb4_general_ci',
+    });
 
-    static associate(db) {
-        db.Comment.belongsTo(db.User, { foreignKey: 'commenter', targetKey: 'id' });
-    }
+    static associate = (db) => db.Comment.belongsTo(db.User, { foreignKey: 'commenter', targetKey: 'id' });
+
 };
--- chapter0-sequlize/models/user.js
+++ chapter0-sequlize/models/user.js
@@ -1,93 +1,40 @@
 const {DataTypes, Model} = require('sequelize');
 
-
 module.exports = class User extends Model {
-    static init(sequelize) {
-        return super.init({
-            name: {
-                type: DataTypes.STRING(20),
-                allowNull: false,
-                unique: true,
-            },
-            age: {
-                type: DataTypes.INTEGER.UNSIGNED,
-                allowNull: false,
-            },
-            married: {
-                type: DataTypes.BOOLEAN,
-                allowNull: false,
-            },
-            comment: {
-                type: DataTypes.TEXT,
-                allowNull: true,
-            },
-            created_at: {
-                type: DataTypes.DATE,
-                allowNull: false,
-                defaultValue: DataTypes.NOW,
-            },
-        }, {
-            sequelize,
-            timestamps: false,  //createdAt, updatedAt
-            underscored: false,
-            modelName: 'User',  // Post
-            tableName: 'users', // posts
-            paranoid: false,    //deletedAt : soft delete
-            charset: 'utf8',
-            collate: 'utf8_general_ci',
-        });
-    }
+    static init = (sequelize) => super.init({
+        name: {
+            type: DataTypes.STRING(20),
+            allowNull: false,
+            unique: true,
+        },
+        age: {
+            type: DataTypes.INTEGER.UNSIGNED,
+            allowNull: false,
+        },
+        married: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+        },
+        comment: {
+            type: DataTypes.TEXT,
+            allowNull: true,
+        },
+        created_at: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+        },
+    }, {
+        sequelize,
+        timestamps: false,  //createdAt, updatedAt
+        underscored: false,
+        modelName: 'User',  // Post
+        tableName: 'users', // posts
+        paranoid: false,    //deletedAt : soft delete
+        charset: 'utf8',
+        collate: 'utf8_general_ci',
+    });
 
-    static associate(db) {
-        db.User.hasMany(db.Comment, { foreignKey: 'commenter', sourceKey: 'id' });
-    }
+    static associate = (db) => db.User.hasMany(db.Comment, { foreignKey: 'commenter', sourceKey: 'id' });
+
 };
-
-
-/*
-module.exports = (sequelize) => {
-    class User extends Model {
-        static associate(db) {
-            this.hasMany(db.Comment, { foreignKey: 'commenter', sourceKey: 'id' })
-        }
-    }
-
-    User.init(
-        {
-            name: {
-                type: Sequelize.STRING(20),
-                allowNull: false,
-                unique: true,
-            },
-            age: {
-                type: Sequelize.INTEGER.UNSIGNED,
-                allowNull: false,
-            },
-            married: {
-                type: Sequelize.BOOLEAN,
-                allowNull: false,
-            },
-            comment: {
-                type: Sequelize.TEXT,
-                allowNull: true,
-            },
-            created_at: {
-                type: Sequelize.DATE,
-                allowNull: false,
-                defaultValue: Sequelize.NOW,
-            }
-        }, {
-            sequelize,
-            timestamps: false,  //createdAt, updatedAt
-            underscored: false,
-            modelName: 'User',  // Post
-            tableName: 'users', // posts
-            paranoid: false,    //deletedAt : soft delete
-            charset: 'utf8',
-            collate: 'utf8_general_ci',
-        }
-    );
-
-    return User;
-}
-*/
